e1e14690d1673ee0f57be91c4f75bb64eeeb04ba
farhan27007 <muhammadfarhan27007@gmail.com>
2022-12-12 09:58:45 +0700

    add login layout

--- application/views/Login.php
+++ application/views/Login.php
@@ -7,6 +7,7 @@
 	<title>jQuery UI Draggable - Default functionality</title>
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
 	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?= base_url('assets/css/gaya.css') ?>">
 	<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 	<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
@@ -16,17 +17,32 @@
 
 <body>
 	<input id="baseurl" type="hidden" value="<?= base_url() ?>">
-	
-    <form action="<?= base_url('/Login/cek_login') ?>" method="post">
-        <input type="email" name="email">
-        <input type="password" name="password">
-        <input type="submit" value="Login">
-    </form>
+	<div class="global-container">
+		<div class="card login-form">
+			<div class="card-body">
+				<h3 class="card-title text-center font-weight-bold">Silahkan Login</h3>
+				<div class="card-text">
+					<form action="<?= base_url('/Login/cek_login') ?>" method="post">
+						<div class="form-group">
+							<label for="exampleInputEmail1">Alamat Email</label>
+							<input type="email" class="form-control form-control-sm" name="email">
+						</div>
+						<div class="form-group">
+							<label for="exampleInputPassword1">Password</label>
+							<input type="password" class="form-control form-control-sm" name="password">
+						</div>
+						<button type="submit" class="btn btn-primary btn-block" value="Login">Login</button>
+					</form>
+					<div class="notif mt-3 pb-2 text-center">
+						<?php echo $this->session->flashdata('pesan'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-    <div class="notif">
-        <?php echo $this->session->flashdata('pesan'); ?>
-    </div>
 
 </body>
 
 </html>
+
